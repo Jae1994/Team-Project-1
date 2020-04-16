b0f6a0e2ea92aea33c934c2cb110ff157cbfc985
Jae1994 <63680414+Jae1994@users.noreply.github.com>
2020-04-16 15:30:31 -0700

    Update FamilyMain.java

--- FamilyMain.java
+++ FamilyMain.java
@@ -1,11 +1,27 @@
-public class FamilyMain{
+import java.io.*;
+import java.util.*;
 
-   public static void main(String[] args){
-      Person HenryVIII = new Person();
-      
-      HenryVIII.getPaternal();
-      HenryVIII.getMaternal();
-      HenryVIII.getChildren();
-      
-   }
+public class FamilyMain {
+	public static void main(String[] args) throws FileNotFoundException {
+		Scanner scan = new Scanner(new File("tudor.dat"));
+		// method assigning names from the file to person
+		
+		ArrayList<Person> names =new ArrayList<Person>();
+		create(scan, names);
+		printList(names);
+	}
+	
+	public static void create(Scanner scan, ArrayList<Person> n) {
+		String line = scan.nextLine();
+		while(!line.equals("END")) {
+			n.add(new Person(line));
+			line = scan.nextLine();
+		}
+		
+	}
+	public static void printList(ArrayList<Person> names){
+		for(Person p: names) {
+			System.out.println(p.getName());
+		}
+	}
 }
